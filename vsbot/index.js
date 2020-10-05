244ba38d859fde7c3abe3a4996018bbae6bca49c
이름 <babsune81@gmail.com>
2020-10-05 12:14:19 +0900

    bot upload

--- vsbot/index.js
+++ vsbot/index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = process.argv.length == 2 ? process.env.token : 'NzQ5NTY1MDcyMzg4NTIyMDE0.X0t03w.Qpi8D5z4mYjWWwzNFw2AYubrJJo';
+const token = process.env.token;'';
 const welcomeChannelName = "welcome";
 const byeChannelName = "welcome";
 const welcomeChannelComment = "어서오세요.";
@@ -31,7 +31,7 @@ client.on("guildMemberRemove", (member) => {
 client.on('message', (message) => {
   if(message.author.bot) return;
 
-  if(message.content == 'ㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔ') {
+  if(message.content == '넵') {
     return message.reply('명령어(뭐이짜슥아...도배 정지 줄까?');
   }  if(message.content == '!구걸') {
     return message.reply('구걸완료.성공됌✅');
@@ -93,13 +93,14 @@ client.on('message', (message) => {
     return message.reply('.............');
   }     if(message.content == '!상점') {
     return message.reply('신느봇사이트로 전해주세요 사이트:https://discord.gg/c2C2JP');
-  } 
+  }  
+
   if(message.content == 'embed') {
     let img = 'https://cdn.discordapp.com/icons/419671192857739264/6dccc22df4cb0051b50548627f36c09b.webp?size=256';
     let embed = new Discord.RichEmbed()
       .setTitle('타이틀')
       .setURL('http://www.naver.com')
-      .setAuthor('신느님', img, 'http://www.naver.com')
+      .setAuthor('디스코드 봇', img, 'http://www.naver.com')
       .setThumbnail(img)
       .addBlankField()
       .addField('Inline field title', 'Some value here')
@@ -109,7 +110,7 @@ client.on('message', (message) => {
       .addField('Inline field title', 'Some value here1\nSome value here2\nSome value here3\n')
       .addBlankField()
       .setTimestamp()
-      .setFooter('나긋해가 만듬', img)
+      .setFooter('디스코드 봇(이)가 만듬', img)
 
     message.channel.send(embed)
   } else if(message.content == 'embed2') {
@@ -122,9 +123,9 @@ client.on('message', (message) => {
     ];
     let commandStr = '';
     let embed = new Discord.RichEmbed()
-      .setAuthor('Help of 콜라곰 BOT', helpImg)
+      .setAuthor('Help of 디스코드 BOT', helpImg)
       .setColor('#186de6')
-      .setFooter(`콜라곰 BOT ❤️`)
+      .setFooter(`디스코드 BOT ❤️`)
       .setTimestamp()
     
     commandList.forEach(x => {
@@ -150,43 +151,6 @@ client.on('message', (message) => {
       return message.reply('채널에서 실행해주세요.');
     }
   }
-
-  if(message.content.startsWith('!청소')) {
-    if(checkPermission(message)) return
-
-    var clearLine = message.content.slice('!청소 '.length);
-    var isNum = !isNaN(clearLine)
-
-    if(isNum && (clearLine <= 0 || 100 < clearLine)) {
-      message.channel.send("1부터 100까지의 숫자만 입력해주세요.")
-      return;
-    } else if(!isNum) { // c @나긋해 3
-      if(message.content.split('<@').length == 2) {
-        if(isNaN(message.content.split(' ')[2])) return;
-
-        var user = message.content.split(' ')[1].split('<@!')[1].split('>')[0];
-        var count = parseInt(message.content.split(' ')[2])+1;
-        const _limit = 10;
-        let _cnt = 0;
-
-        message.channel.fetchMessages({limit: _limit}).then(collected => {
-          collected.every(msg => {
-            if(msg.author.id == user) {
-              msg.delete();
-              ++_cnt;
-            }
-            return !(_cnt == count);
-          });
-        });
-      }
-    } else {
-      message.channel.bulkDelete(parseInt(clearLine)+1)
-        .then(() => {
-          AutoMsgDelete(message, `<@${message.author.id}> ` + parseInt(clearLine) + "개의 메시지를 삭제했습니다. (이 메세지는 잠시 후에 사라집니다.)");
-        })
-        .catch(console.error)
-    }
-  }
 });
 
 function checkPermission(message) {
@@ -207,14 +171,6 @@ function changeCommandStringLength(str, limitLen = 8) {
   }
 
   return tmp;
-}
-
-async function AutoMsgDelete(message, str, delay = 3000) {
-  let msg = await message.channel.send(str);
-
-  setTimeout(() => {
-    msg.delete();
-  }, delay);
 }
 
 
