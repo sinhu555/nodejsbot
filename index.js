4f545577c496c6ffd37f06debea5641b67d23561
이름 <babsune81@gmail.com>
2020-10-05 16:58:06 +0900

    add serverinfo

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = process.argv.length == 2 ? process.env.token : "";
+const token = process.argv.length == 2 ? process.env.token : "'";
 const moment = require("moment");
 require("moment-duration-format");
 const welcomeChannelName = "welcome";
